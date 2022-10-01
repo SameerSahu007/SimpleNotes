0a454b65f08fe05947571c45e3d0892d4ac5a716
kai <kailashjat604@gmail.com>
2022-10-01 10:51:48 +0530

    Remove unnecessary conditional statements

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,3 @@
-import re
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from .forms import PostForm
@@ -12,15 +11,13 @@ from django.contrib.auth import logout
 
 @login_required
 def mynotes(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            temp = form.save(commit=False)
-            temp.user = request.user
-            temp.save()
-            return redirect('/')
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        temp = form.save(commit=False)
+        temp.user = request.user
+        temp.save()
+        return redirect('/')
 
-    form = PostForm()
     current_user = User.objects.get(username=request.user.username)
     allposts = Post.objects.filter(user=current_user)
 
@@ -33,21 +30,18 @@ def mynotes(request):
 
 
 def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created for {username}')
-            return redirect('login')
-    else:
-        form = UserCreationForm()
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        messages.success(request, f'Account Created for {username}')
+        return redirect('login')
+
     return render(request, 'notes/signup.html', {'form': form})
 
 
 @login_required
 def delete(request, id):
-    print(id)
     current_user = User.objects.get(username=request.user.username)
     Post.objects.filter(user=current_user, id=id).delete()
     return redirect('/')
